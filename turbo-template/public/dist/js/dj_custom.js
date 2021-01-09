d00579c3c1f388d78e729692d2cf70560b98caf1
David Carrillo Jr <djcarrillo6@yahoo.com>
2021-01-08 20:06:35 -0800

    Animated gradients & color theme changes

---
+++ turbo-template/public/dist/js/dj_custom.js
@@ -0,0 +1,29 @@
+// - Netflix Navigation -
+const open_btn = document.querySelector('.open-btn');
+const close_btn = document.querySelector('.close-btn');
+const nav = document.querySelectorAll('.nav');
+
+open_btn.addEventListener('click', () => {
+    nav.forEach(nav_el => nav_el.classList.add('visible'));
+});
+
+close_btn.addEventListener('click', () => {
+    nav.forEach(nav_el => nav_el.classList.remove('visible'));
+});
+
+
+// 3D Boxes JS
+const boxesContainer = document.getElementById('boxes');
+
+function createBoxes() {
+    for (let i = 0; i < 4; i++) {
+        for (let j = 0; j < 4; j++) {
+            const box = document.createElement('div');
+            box.classList.add('box');
+            box.style.backgroundPosition = `${-j * 125}px ${-i * 125}px`;
+            boxesContainer.appendChild(box);
+        };
+    };
+};
+
+createBoxes();
